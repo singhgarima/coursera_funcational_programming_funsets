023835c3604629f825df352881dda7665cb6988f
Garima Singh <igarimasingh@gmail.com>
2014-05-22 21:40:20 +0800

    Initial Commit

---
+++ src/test/scala/funsets/FunSetSuite.scala
@@ -0,0 +1,78 @@
+package funsets
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * This class is a test suite for the methods in object FunSets. To run
+ * the test suite, you can either:
+ *  - run the "test" command in the SBT console
+ *  - right-click the file in eclipse and chose "Run As" - "JUnit Test"
+ */
+@RunWith(classOf[JUnitRunner])
+class FunSetSuite extends FunSuite {
+
+    import FunSets._
+
+  test("contains") {
+    assert(contains((elem => elem < 0), -1))
+    assert(!contains((elem => elem < 0), 1))
+  }
+
+  trait TestSets {
+    val s1 = singletonSet(1)
+    val s2 = singletonSet(2)
+    val s3 = singletonSet(3)
+  }
+
+  test("singletonSet(1) contains 1") {
+    new TestSets {
+      assert(contains(s1, 1), "Singleton class should contain 1")
+      assert(!contains(s1, 2), "Singleton class should not contain 2")
+    }
+  }
+
+  test("union contains all elements") {
+    new TestSets {
+      val s = union(s1, s2)
+      assert(contains(s, 1), "Union should contain 1")
+      assert(contains(s, 2), "Union should conatin 2")
+      assert(!contains(s, 3), "Union should not contain 3")
+    }
+  }
+  
+  test("intersect contains comman elements") {
+    new TestSets {
+      val s = intersect(union(s1, s2), s1)
+      assert(contains(s, 1), "Union should contain 1")
+      assert(!contains(s, 2), "Union should not conatin 2")
+      assert(!contains(s, 3), "Union should not contain 3")
+    }
+  }
+  
+  test("diff contains element in first set which are not in set 2") {
+    new TestSets {
+      val s = diff(union(s1, s2), s1)
+      assert(!contains(s, 1), "Union should not contain 1")
+      assert(contains(s, 2), "Union should conatin 2")
+      assert(!contains(s, 3), "Union should not contain 3")
+    }
+  }
+  
+  test("filter removes element from set which dont satisfy the condition") {
+    new TestSets {
+      val s = filter((elem => elem > 0 & elem < 4), x => x < 3)
+      assert(contains(s, 1), "Union should contain 1")
+      assert(contains(s, 2), "Union should conatin 2")
+      assert(!contains(s, 3), "Union should not contain 3")
+      assert(!contains(s, 4), "Union should not contain 4")
+    }
+  }
+  
+  test("forall") {
+    assert(!forall((elem => elem < 10 & elem > 0), elem => elem < 2))
+    assert(forall((elem => elem < 10 & elem > 0), elem => elem < 11))
+  }
+}
